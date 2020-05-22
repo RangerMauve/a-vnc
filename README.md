14774a59f5ca8e49014ed64d6ba6ada90e418530
mauve <ranger@mauve.moe>
2020-05-21 22:35:44 -0400

    Progress!

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # a-vnc
 Render VNC terminals using Aframe entities
+
+## Quick start
+
+- clone the repo
+- Install x11vnc
+- Run this command to start the server `x11vnc -avahi -noscr -loop -shared -forever -auth guess -reopen -find -nolookup`
+- Or run another VNC server / websockify proxy accoding to the [noVNC docs](https://novnc.com/info.html)
+- Modify `index.html` to point to your server's IP:port
+- Serve `index.html` in a local webserver
+- Open in your headset to see the screen.
+- Mess around with the display resolution / texture size until you're comfortable
+
--- a-vnc.js
+++ a-vnc.js
@@ -7,41 +7,61 @@ AFRAME.registerComponent('vnc', {
     url: { type: 'string' },
     username: { type: 'string' },
     password: { type: 'string' },
-    target: { type: 'string' },
+    target: { type: 'string' }
   },
 
   async init () {
-  	console.log('Beginning connection', this.data)
+    console.log('Beginning connection', this.data)
     const {
       url,
       username,
       password,
-      target,
+      target
     } = this.data
 
     this.vncIndex = VNC_INSTANCE_INDEX++
 
     const element = document.createElement('div')
 
-    element.style = `display: none;`
+    element.style = 'display: none;'
 
     this.el.appendChild(element)
-		const {default:RFB} = await import('https://novnc.com/noVNC/core/rfb.js')
+    const { default: RFB } = await import('https://novnc.com/noVNC/core/rfb.js')
+    const { default: Keyboard } = await import('https://novnc.com/noVNC/core/input/keyboard.js')
 
     this.rfb = new RFB(element, url, { username, password, target })
+    this.keyboard = new Keyboard(document.body)
 
     const canvas = this.rfb._canvas
 
     canvas.id = `vnc-canvas-${this.vncIndex}`
 
-		this.rfb.addEventListener('connect', () => {
-		setTimeout(() => {
-			this.el.setAttribute('material', {
-				src: `#${canvas.id}`,
-				width: 800,
-				height: 600
-			})
-		}, 5000)
-		})
+    this.keyboard.onkeyevent = (keysym, code, down) => {
+      console.log('Keyboard event', { keysym, code, down })
+      this.rfb.sendKey(keysym, code, down)
+    }
+
+    this.updateMaterial = () => {
+      const el = this.el
+      const material = el.getObject3D('mesh').material
+      if (!material.map) return
+      material.map.needsUpdate = true
+      window.requestAnimationFrame(this.updateMaterial)
+    }
+    this.rfb.addEventListener('connect', () => {
+      setTimeout(() => {
+        this.el.setAttribute('material', {
+          src: `#${canvas.id}`,
+          width: 800,
+          height: 600
+        })
+        this.keyboard.grab()
+      }, 1000)
+    })
   },
+
+  remove () {
+    this.updateMaterial = () => undefined
+    this.rfb.disconnect()
+  }
 })
